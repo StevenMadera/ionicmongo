d803260b4c4160ba4118111385c7bec04449eb95
StevenMadera <maderasteven4@gmail.com>
2025-09-28 18:32:05 -0500

    Agrego ruta de bienvenida en /

--- ionicmongo-backend/index.js
+++ ionicmongo-backend/index.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API de IonicMongo funcionando');
+});
 app.use('/api', require('./routes/protected'));
 app.use('/api/expenses', require('./routes/expenses'));
 
